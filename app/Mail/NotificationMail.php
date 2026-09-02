9d4b462e66e8426dbb7ef00d27a5a007cd635abe
greycaleb <caleb@digiversesolutonsug.com>
2026-09-02 14:03:28 +0300

    fix(mail): send account setup and notification emails synchronously to prevent queue backlog

--- app/Mail/NotificationMail.php
+++ app/Mail/NotificationMail.php
@@ -5,13 +5,12 @@ namespace App\Mail;
 use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationMail extends Mailable implements ShouldQueue
+class NotificationMail extends Mailable
 {
     use Queueable, SerializesModels;
 
--- app/Mail/ResetPasswordMail.php
+++ app/Mail/ResetPasswordMail.php
@@ -4,13 +4,12 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ResetPasswordMail extends Mailable implements ShouldQueue
+class ResetPasswordMail extends Mailable
 {
     use Queueable, SerializesModels;
 
